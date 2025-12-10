a8e2af4d500267bc0f24dbb01d5a81dc8882b9d3
Mohit Parmar <127684291+mrmohitparmar@users.noreply.github.com>
2025-12-10 11:18:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-//your code here
+let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 
+// sort array
+touristSpots.sort();
+
+let ul = document.getElementById("ul");
+
+// loop through array
+for (let i = 0; i < touristSpots.length; i++) {
+    let li = document.createElement("li");  // create li
+    li.textContent = touristSpots[i];       // text add
+    ul.append(li);                          // append to ul
+}
